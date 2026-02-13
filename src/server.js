376d008466540ebca64e92caf1cefdbd2a7fbc28
Divyanshverma1000 <divyanshverma05440@gmail.com>
2026-02-14 03:18:22 +0530

    secure cookies fixed

--- src/server.js
+++ src/server.js
@@ -13,6 +13,8 @@ const publicRoutes = require('./routes/public');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.set('trust proxy', 1);
+
 
 // CORS configuration
 app.use(cors({
